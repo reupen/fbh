53bfd1f14ca694a97489b6218cf2d8325e4c41b6
Reupen Shah <reupen@users.noreply.github.com>
2018-02-11 19:25:39 +0000

    Reformat files using ClangFormat

--- initquit.h
+++ initquit.h
@@ -1,28 +1,23 @@
 #pragma once
 
 namespace fbh {
-    class InitQuitDynamic {
-    public:
-        virtual void on_init() {}
-        virtual void on_quit() {}
-    };
+class InitQuitDynamic {
+public:
+    virtual void on_init() {}
+    virtual void on_quit() {}
+};
 
-    class InitQuitManager : public initquit {
-    public:
-        void register_instance(InitQuitDynamic * ptr)
-        {
-            m_instances.add_item(ptr);
-        }
-        void deregister_instance(InitQuitDynamic * ptr)
-        {
-            m_instances.remove_item(ptr);
-        }
-        static void s_register_instance(InitQuitDynamic * ptr);
-        static void s_deregister_instance(InitQuitDynamic * ptr);
-    private:
-        void on_init() override;
-        void on_quit() override;
+class InitQuitManager : public initquit {
+public:
+    void register_instance(InitQuitDynamic* ptr) { m_instances.add_item(ptr); }
+    void deregister_instance(InitQuitDynamic* ptr) { m_instances.remove_item(ptr); }
+    static void s_register_instance(InitQuitDynamic* ptr);
+    static void s_deregister_instance(InitQuitDynamic* ptr);
 
-        pfc::ptr_list_t<InitQuitDynamic> m_instances;
-    };
-}
+private:
+    void on_init() override;
+    void on_quit() override;
+
+    pfc::ptr_list_t<InitQuitDynamic> m_instances;
+};
+} // namespace fbh
--- main_thread_callback.h
+++ main_thread_callback.h
@@ -1,22 +1,19 @@
 #pragma once
 
 namespace fbh {
-    class LambdaMainThreadCallback : public main_thread_callback {
-    public:
-        LambdaMainThreadCallback(std::function<void()> callback)
-            : m_callback{callback} {}
-    private:
-        void callback_run() override
-        {
-            m_callback();
-        }
+class LambdaMainThreadCallback : public main_thread_callback {
+public:
+    LambdaMainThreadCallback(std::function<void()> callback) : m_callback{callback} {}
 
-        std::function<void()> m_callback;
-    };
+private:
+    void callback_run() override { m_callback(); }
 
-    inline void queue_main_thread_callback(std::function<void()> callback)
-    {
-        service_ptr_t<main_thread_callback> cb = new service_impl_t<LambdaMainThreadCallback>(callback);
-        static_api_ptr_t<main_thread_callback_manager>()->add_callback(cb);
-    }
+    std::function<void()> m_callback;
+};
+
+inline void queue_main_thread_callback(std::function<void()> callback)
+{
+    service_ptr_t<main_thread_callback> cb = new service_impl_t<LambdaMainThreadCallback>(callback);
+    static_api_ptr_t<main_thread_callback_manager>()->add_callback(cb);
 }
+} // namespace fbh
