522d48fbf0bd45cd58c88d5dee3e588bca38103a
Reupen Shah <reupen@users.noreply.github.com>
2019-07-04 19:17:57 +0100

    Add reverse parameter to metadb_handle sorting functions

--- sort.h
+++ sort.h
@@ -4,7 +4,7 @@ namespace fbh {
 template <typename TList>
 void sort_metadb_handle_list_by_format_get_permutation_partial(TList&& p_list, t_size p_list_count, t_size base,
     t_size count, mmh::Permutation& order, const service_ptr_t<titleformat_object>& p_script, titleformat_hook* p_hook,
-    bool b_stablise = false)
+    bool stablise = false, bool reverse = false)
 {
     assert(base + count <= p_list_count);
     pfc::array_t<pfc::array_t<WCHAR>> data;
@@ -21,24 +21,25 @@ void sort_metadb_handle_list_by_format_get_permutation_partial(TList&& p_list, t
         return StrCmpLogicalW(elem1.get_ptr(), elem2.get_ptr());
     };
 
-    mmh::sort_get_permutation(data, order, comparator, b_stablise, false, true);
+    mmh::sort_get_permutation(data, order, comparator, stablise, reverse, true);
 }
 
 template <typename TList>
 void sort_metadb_handle_list_by_format_get_permutation(TList&& p_list, mmh::Permutation& order,
-    const service_ptr_t<titleformat_object>& p_script, titleformat_hook* p_hook, bool b_stablise = false)
+    const service_ptr_t<titleformat_object>& p_script, titleformat_hook* p_hook, bool stablise = false,
+    bool reverse = false)
 {
     sort_metadb_handle_list_by_format_get_permutation_partial(
-        p_list, order.get_count(), 0, order.get_count(), order, p_script, p_hook, b_stablise);
+        p_list, order.get_count(), 0, order.get_count(), order, p_script, p_hook, stablise, reverse);
 }
 
 template <typename TList>
 void sort_metadb_handle_list_by_format(TList&& p_list, const service_ptr_t<titleformat_object>& p_script,
-    titleformat_hook* p_hook, bool b_stablise = false)
+    titleformat_hook* p_hook, bool stablise = false, bool reverse = false)
 {
     mmh::Permutation perm(p_list.get_count());
     sort_metadb_handle_list_by_format_get_permutation_partial(
-        p_list.get_ptr(), p_list.get_count(), 0, perm.get_count(), perm, p_script, p_hook, b_stablise);
+        p_list.get_ptr(), p_list.get_count(), 0, perm.get_count(), perm, p_script, p_hook, stablise, reverse);
     p_list.reorder(perm.get_ptr());
 }
 
