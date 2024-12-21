32a02c8cdd9a4d0fcb2336e95666133e70302171
Reupen Shah <reupen@users.noreply.github.com>
2024-12-21 17:13:41 +0000

    Update InfoBox helpers
    This updates the InfoBox helpers following updates in ui_helpers.

--- info_box.h
+++ info_box.h
@@ -1,10 +1,13 @@
 #pragma once
 
 namespace fbh {
+
 void show_info_box(HWND wnd_parent, const char* p_title, const char* p_text, INT icon = OIC_INFORMATION,
-    uih::alignment text_alignment = uih::ALIGN_LEFT);
+    uih::alignment text_alignment = uih::ALIGN_LEFT,
+    std::function<std::optional<INT_PTR>(HWND, UINT, WPARAM, LPARAM)> on_before_message = nullptr);
 void show_info_box_threadsafe(HWND wnd, const char* p_title, const char* p_text, INT oem_icon = OIC_INFORMATION,
     uih::alignment text_alignment = uih::ALIGN_LEFT);
 void show_info_box_threadsafe(const char* p_title, const char* p_text, INT oem_icon = OIC_INFORMATION,
     uih::alignment text_alignment = uih::ALIGN_LEFT);
+
 } // namespace fbh
